29d0545319df6652cca7234cdc0626d212d9be51
Abdulrazaq Ghabsha <abood.ghabsha@gmail.com>
2023-10-30 20:22:23 -0400

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Salman Ghabsha on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
